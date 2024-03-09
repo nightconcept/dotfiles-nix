684871244ab596442635e4fe5bde189cc50f5024
Danny Solivan <dark@nightconcept.net>
2024-03-09 08:20:04 -0800

    WIP

--- dots/zsh/default.nix
+++ dots/zsh/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, vars, ... }:
+{ pkgs, vars, lib, config, inputs, ... }:
 
 {
   users.users.${vars.user} = {
@@ -6,10 +6,6 @@
   };
 
   programs = {
-    zoxide = {
-      enable = true;
-      enableZshIntegration = true;
-    };
 
     zsh = {
       enable = true;
@@ -32,8 +28,8 @@
         # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
         # Initialization code that may require console input (password prompts, [y/n]
         # confirmations, etc.) must go above this block; everything else may go below.
-        if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-          source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+        if [[ -r "$\{XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-$\{(%):-%n}.zsh" ]]; then
+          source "$\{XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-$\{(%):-%n}.zsh"
         fi
 
         ###################
