921cda1ea04efc440ffa414ff29b7a47fd9c9cb9
Danny <dark@nightconcept.net>
2024-03-09 21:06:27 -0800

    Re-add missing fig header to zshrc

--- dots/zsh/default.nix
+++ dots/zsh/default.nix
@@ -9,7 +9,9 @@
         ########################
         # Headers (do not touch)
         ########################
-
+        
+        # Fig pre block. Keep at the top of this file.
+        [[ -f "$HOME/.fig/shell/zshrc.pre.zsh" ]] && builtin source "$HOME/.fig/shell/zshrc.pre.zsh"
         # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
         # Initialization code that may require console input (password prompts, [y/n]
         # confirmations, etc.) must go above this block; everything else may go below.
@@ -39,7 +41,6 @@
 
         eval "$(pyenv init -)"
         eval "$(fnm env --use-on-cd)"
-        eval "$(zoxide init zsh)"
         eval "$(thefuck --alias)"
         if [ -d "/home/linuxbrew/" ]; then
             eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
@@ -151,7 +152,10 @@
 
         # Fig post block. Keep at the bottom of this file.
         if [ -d "$HOME/.fig/" ]; then
-          fi
+          [[ -f "$HOME/.fig/shell/zshrc.post.zsh" ]] && builtin source "$HOME/.fig/shell/zshrc.post.zsh"
+        fi
+
+        eval "$(zoxide init zsh)"
       '';
     };
   };
