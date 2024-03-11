bf7e6096e44265e0dbf2d3047fe3c018f65d79c1
Danny Solivan <dark@nightconcept.net>
2024-03-10 21:04:05 -0700

    Working vscode, but not 100% correct

--- dots/vscode/default.nix
+++ dots/vscode/default.nix
@@ -1,20 +1,17 @@
 { inputs, pkgs, lib, ... }:
-
 {
   programs = {
     vscode = {
       enable = true;
       extensions = with pkgs.vscode-extensions; [
         bbenoist.nix
-        sherloach.solarized-osaka
       ];
       userSettings = {
-        "workbench.colorTheme": "Solarized Osaka",
-        "editor.fontFamily": "'FiraMono Nerd Font', 'monospace', monospace",
-        "editor.fontSize": 16;
-        "editor.fontLigatures": false;
-      }
-    }
-
-  }
+        "workbench.colorTheme" = "Solarized Osaka";
+        "editor.fontFamily" = "'FiraMono Nerd Font', 'monospace', monospace";
+        "editor.fontSize" = 16;
+        "editor.fontLigatures" = false;
+      };
+    };
+  };
 }
