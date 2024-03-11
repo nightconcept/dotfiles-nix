1c76c923c77f60ad757af7350748d1186d0faa84
Danny Solivan <dark@nightconcept.net>
2024-03-11 14:05:05 -0700

    Attempt to add vs code extensions not listed in vscode-extensions

--- dots/vscode/default.nix
+++ dots/vscode/default.nix
@@ -4,7 +4,15 @@
     vscode = {
       enable = true;
       extensions = with pkgs.vscode-extensions; [
+        yzhang.markdown-all-in-one
         bbenoist.nix
+      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "solarized-osaka";
+        publisher = "sherloach";
+        version = "0.1.1";
+        sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+      }
       ];
       userSettings = {
         "workbench.colorTheme" = "Solarized Osaka";
