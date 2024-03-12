5d90294ffc04a827e6accaf934680233c91858e7
Danny Solivan <dark@nightconcept.net>
2024-03-11 21:03:48 -0700

    More vsc extensions

--- dots/vscode/default.nix
+++ dots/vscode/default.nix
@@ -13,12 +13,25 @@
         version = "0.1.1";
         sha256 = "sha256-HYkzht8jPYBwE3bHHvyU4amNYunsfayPTWBiBVyY+1g=";
       }
+      {
+        name = "where-am-i";
+        publisher = "antfu";
+        version = "0.2.0";
+        sha256 = "sha256-M9TCILD6KKLHCDBP0mBR5soeYb2MFuBAmyKPlKbl1tg=";
+      }
+      {
+        name = "material-icon-theme";
+        publisher = "PKief";
+        version = "4.34.0";
+        sha256 = "sha256-xxOEUvMjqJbl8lONB/So2NoIAVPOxysTq2YQY3iHGqo=";
+      }
       ];
       userSettings = {
         "workbench.colorTheme" = "Solarized Osaka";
         "editor.fontFamily" = "'FiraMono Nerd Font', 'monospace', monospace";
         "editor.fontSize" = 16;
         "editor.fontLigatures" = false;
+        "workbench.iconTheme" = "material-icon-theme";
       };
     };
   };
