443cabf0aba7d0fa35795a04c5399fbececc867f
Danny Solivan <dark@nightconcept.net>
2024-03-14 20:30:32 -0700

    Add basic setup script for bootstrapping nix for non-macOS/NixOS systems

---
+++ bootstrap.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# install zsh
+sudo apt-get install zsh -y
+
+# install nix
+curl --proto '=https' --tlsv1.2 -sSf -L \
+  https://install.determinate.systems/nix | sh -s -- install
+
+# install home manager standalone
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
+nix-shell '<home-manager>' -A install
+
+# get dotfiles
+git clone https://github.com/nightconcept/dotfiles-nix
