e49eb1b622dc1429c622e67c2e265f0aef58f45e
Danny Solivan <dark@nightconcept.net>
2024-03-15 17:54:20 -0700

    Update home-persist.nix

--- users/danny/home-persist.nix
+++ users/danny/home-persist.nix
@@ -5,6 +5,68 @@
     inputs.impermanence.nixosModules.home-manager.impermanence
   ];
 
+  programs.home-manager.enable = true;
+
+  nixpkgs.config.allowUnfree = true;
+
+  fonts.fontconfig.enable = true;
+
+  home.packages = with pkgs; [
+    duf
+    eza
+    fd
+    fnm
+    fzf
+    lazygit
+    ncdu
+    neofetch
+    neovim
+    nmap
+    pyenv
+    rsync
+    speedtest-cli
+    stow
+    thefuck
+    tldr
+    tmux
+    trash-cli
+    wget
+    zip
+    zoxide
+
+    audacious
+    bandwhich
+    corectrl
+    calibre
+    discord
+    foliate
+    hugo
+    obsidian
+    steam
+    evince
+    fontconfig
+    ferdium
+    fnm
+    github-desktop
+    hexchat
+    libreoffice-fresh
+    mpv
+    nomachine-client
+    pavucontrol
+    protonup-qt
+    spotify
+    stretchly
+    ungoogled-chromium
+    vscode
+    wezterm
+    zoom
+  ];
+
+  imports = [
+      ../../dots/zsh
+      ../../dots/vscode
+  ];
+
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
   home.persistence."/persist/home" = {
@@ -30,37 +92,4 @@
     ];
     allowOther = true;
   };
-
-  programs.home-manager.enable = true;
-
-  nixpkgs.config.allowUnfree = true;
-
-  fonts.fontconfig.enable = true;
-
-  home.packages = with pkgs; [
-    duf
-    eza
-    fd
-    fnm
-    fzf
-    lazygit
-    ncdu
-    fastfetch
-    neovim
-    nmap
-    pyenv
-    rsync
-    speedtest-cli
-    stow
-    thefuck
-    tldr
-    tmux
-    trash-cli
-    wget
-    zip
-    zoxide
-
-    vscode
-    wezterm
-  ];
 }
