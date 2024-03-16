579555033fa2bd0c9ae55fcccf35c161063a9e4a
Danny Solivan <dark@nightconcept.net>
2024-03-15 20:54:38 -0700

    Add Plasma and Firefox directories to persist

--- users/danny/home-persist.nix
+++ users/danny/home-persist.nix
@@ -63,7 +63,7 @@
     wezterm
     zoom
   ];
-  
+
   home.persistence."/persist/home" = {
     directories = [
       "Downloads"
@@ -88,6 +88,105 @@
     ];
     allowOther = true;
   };
+
+    home.persistence."/persist/dotfiles-persist" = {
+      removePrefixDirectory = true;   # for GNU Stow styled dotfile folders
+      allowOther = true;
+      directories = [
+        # fuse mounted from /nix/dotfiles/Firefox/.mozilla to /home/$USERNAME/.mozilla
+        "Firefox/.mozilla"
+      ];
+    };
+
+    # fricking KDE Plasma has a bazillion files and each needs to be linked individually
+    # because they're all just shoved into ~/.config and not into a single folder.
+    # It's separate from the other dotfiles so I can write ".config"
+    # instead of "Plasma/.config"
+    home.persistence."/persist/dotfiles-persist/Plasma" = {
+      removePrefixDirectory = false;
+      allowOther = true;
+      directories = [
+        ".config/gtk-3.0"   # fuse mounted from /nix/dotfiles/Plasma/.config/gtk-3.0
+        ".config/gtk-4.0"     # to /home/$USERNAME/.config/gtk-3.0
+        ".config/KDE"
+        ".config/kde.org"
+        ".config/plasma-workspace"
+        ".config/xsettingsd"
+        ".kde"
+
+        ".local/share/baloo"
+        ".local/share/dolphin"
+        ".local/share/kactivitymanagerd"
+        ".local/share/kate"
+        ".local/share/klipper"
+        ".local/share/konsole"
+        ".local/share/kscreen"
+        ".local/share/kwalletd"
+        ".local/share/kxmlgui5"
+        ".local/share/RecentDocuments"
+        ".local/share/sddm"
+      ];
+      files = [
+        ".config/akregatorrc"
+        ".config/baloofileinformationrc"
+        ".config/baloofilerc"
+        ".config/bluedevilglobalrc"
+        ".config/device_automounter_kcmrc"
+        ".config/dolphinrc"
+        ".config/filetypesrc"
+        ".config/gtkrc"
+        ".config/gtkrc-2.0"
+        ".config/gwenviewrc"
+        ".config/kactivitymanagerd-pluginsrc"
+        ".config/kactivitymanagerd-statsrc"
+        ".config/kactivitymanagerd-switcher"
+        ".config/kactivitymanagerdrc"
+        ".config/katemetainfos"
+        ".config/katerc"
+        ".config/kateschemarc"
+        ".config/katevirc"
+        ".config/kcmfonts"
+        ".config/kcminputrc"
+        ".config/kconf_updaterc"
+        ".config/kded5rc"
+        ".config/kdeglobals"
+        ".config/kgammarc"
+        ".config/kglobalshortcutsrc"
+        ".config/khotkeysrc"
+        ".config/kmixrc"
+        ".config/konsolerc"
+        ".config/kscreenlockerrc"
+        ".config/ksmserverrc"
+        ".config/ksplashrc"
+        ".config/ktimezonedrc"
+        ".config/kwinrc"
+        ".config/kwinrulesrc"
+        ".config/kxkbrc"
+        ".config/mimeapps.list"
+        ".config/partitionmanagerrc"
+        ".config/plasma-localerc"
+        ".config/plasma-nm"
+        ".config/plasma-org.kde.plasma.desktop-appletsrc"
+        ".config/plasmanotifyrc"
+        ".config/plasmarc"
+        ".config/plasmashellrc"
+        ".config/PlasmaUserFeedback"
+        ".config/plasmawindowed-appletsrc"
+        ".config/plasmawindowedrc"
+        ".config/powermanagementprofilesrc"
+        ".config/spectaclerc"
+        ".config/startkderc"
+        ".config/systemsettingsrc"
+        ".config/Trolltech.conf"
+        ".config/user-dirs.dirs"
+        ".config/user-dirs.locale"
+
+        ".local/share/krunnerstaterc"
+        ".local/share/user-places.xbel"
+        ".local/share/user-places.xbel.bak"
+        ".local/share/user-places.xbel.tbcache"
+      ];
+    };
 
   # Do not touch
   home.stateVersion = "23.11";
