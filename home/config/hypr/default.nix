cf6aa99aaa7de557e848058ec8ad6a154fe66b08
Danny Solivan <dark@nightconcept.net>
2024-03-17 14:46:31 -0700

    Update default.nix

--- home/config/hypr/default.nix
+++ home/config/hypr/default.nix
@@ -19,7 +19,7 @@
   #test later systemd.user.targets.hyprland-session.Unit.Wants = [ "xdg-desktop-autostart.target" ];
   wayland.windowManager.hyprland = {
     enable = true;
-    reloadConfig
+    reloadConfig = true;
     recommendedEnvironment = true;
     systemdIntegration = true;
 
