cfb303de3abbfeebc0511283a32ed9de5a3d4a67
Danny Solivan <dark@nightconcept.net>
2024-03-18 14:21:14 -0700

    WIP

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -5,11 +5,62 @@
   ...
 }: {
   imports = [
-    ./dunst
     ./git.nix
-    ./hypr
-    ./rofi
-    ./waybar
-    ./wlogout
+    ./vscode
+    #./wezterm
+    ./zsh
+  ];
+
+  home.packages = with pkgs; [
+    duf
+    dunst
+    eza
+    fd
+    fnm
+    fzf
+    ncdu
+    neofetch
+    neovim
+    nmap
+    pyenv
+    rofi
+    rsync
+    speedtest-cli
+    stow
+    thefuck
+    tldr
+    tmux
+    trash-cli
+    wget
+    zip
+    zoxide
+
+    audacious
+    bandwhich
+    corectrl
+    calibre
+    discord
+    foliate
+    hugo
+    obsidian
+    steam
+    evince
+    fontconfig
+    ferdium
+    fnm
+    github-desktop
+    hexchat
+    libreoffice-fresh
+    mpv
+    nomachine-client
+    pavucontrol
+    protonup-qt
+    spotify
+    stretchly
+    ungoogled-chromium
+    vscode
+    waybar
+    wezterm
+    zoom
   ];
 }
