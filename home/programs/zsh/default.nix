3078f9b39e6ed6ad1dae978bc268fa21c5c04931
Danny Solivan <dark@nightconcept.net>
2024-03-20 15:57:01 -0700

    Remove gh auth on zsh startup

--- home/programs/zsh/default.nix
+++ home/programs/zsh/default.nix
@@ -56,8 +56,6 @@
         if [[ -r "$\{XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-$\{(%):-%n}.zsh" ]]; then
           source "$\{XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-$\{(%):-%n}.zsh"
         fi
-
-        gh auth setup-git
       '';
       
       initExtra = ''
