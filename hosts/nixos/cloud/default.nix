7fe141be567acf9534a5d93de7932a13b76f21e8
Danny Solivan <dark@nightconcept.net>
2024-03-19 20:30:58 -0700

    Refactor cloud-next into cloud

--- hosts/nixos/cloud/default.nix
+++ hosts/nixos/cloud/default.nix
@@ -1,9 +1,15 @@
-{ config, pkgs, ... }:
 {
-  imports =
-    [
-      ./hardware-configuration.nix
-    ];
+  config,
+  pkgs,
+  lib,
+  inputs,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+    inputs.disko.nixosModules.default
+    (import ../disko.nix {device = "/dev/nvme0n1";})
+  ];
 
   # Kernel specified at 6.6 for the latest LTS
   boot.kernelPackages = pkgs.linuxPackages_6_6;
@@ -11,19 +17,37 @@
   networking.hostName = "cloud";
 
   services.xserver.enable = true;
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
-
+  services.xserver.displayManager.gdm.enable = true;
   services.xserver = {
     xkb.layout = "us";
     xkb.variant = "";
   };
 
+  # hyprland
+  programs.hyprland = {
+    enable = true;
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    xwayland.enable = true;
+  };
+  # for hyprland
+  services.dbus.enable = true;
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+    ];
+  };
+  # Hint electron apps to use wayland
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+  };
+
   environment.systemPackages = with pkgs; [
     acpi
     firefox
-    home-manager
     powertop
+    networkmanagerapplet
   ];
 
   # Do not touch
