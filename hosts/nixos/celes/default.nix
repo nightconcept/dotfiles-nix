b964ea613377864357545387e9afa7348c5b7e4d
Danny Solivan <dark@nightconcept.net>
2024-03-20 19:58:16 -0700

    Add automount USB

--- hosts/nixos/celes/default.nix
+++ hosts/nixos/celes/default.nix
@@ -48,6 +48,13 @@
     home-manager
   ];
 
+  services.udev.enable = true;
+  # Automount USB drives
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEMS=="usb", SUBSYSTEM=="block", ENV{ID_FS_USAGE}=="filesystem",
+    RUN{program} += "${pkgs.systemd}/bin/systemd-mount --no-block --automount=yes --collect $devnode /media
+  '';
+
   # Do not touch
   system.stateVersion = "23.11";
 }
