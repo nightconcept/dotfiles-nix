a0ce0c9864f6449bea2a3a2d2615b49dbe1d65c0
Danny Solivan <dark@nightconcept.net>
2024-03-20 18:23:39 -0700

    Add more wayland env variables

--- home/desktops/hyprland/hyprland/hyprland-environment.nix
+++ home/desktops/hyprland/hyprland/hyprland-environment.nix
@@ -8,14 +8,22 @@
       EDITOR = "nvim";
       BROWSER = "firefox";
       TERMINAL = "wezterm";
-      WLR_NO_HARDWARE_CURSORS = "1";
-      WLR_RENDERER_ALLOW_SOFTWARE = "1";
+
+      # https://wiki.hyprland.org/Configuring/Environment-variables/
+      GDK_BACKEND = "wayland";
+      QT_QPA_PLATFORM = "wayland";
+      SDL_VIDEODRIVER = "wayland";
       CLUTTER_BACKEND = "wayland";
-      WLR_RENDERER = "vulkan";
 
       XDG_CURRENT_DESKTOP = "Hyprland";
       XDG_SESSION_DESKTOP = "Hyprland";
       XDG_SESSION_TYPE = "wayland";
+
+      WLR_NO_HARDWARE_CURSORS = "1";
+      WLR_RENDERER_ALLOW_SOFTWARE = "1";
+
+      WLR_RENDERER = "vulkan";
+      ELECTRON_OZONE_PLATFORM_HINT = "auto";
     };
   };
 }
