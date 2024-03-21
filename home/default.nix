c4f2142cf9bf61143c6a991a9c9ea47334f4ff49
Danny Solivan <dark@nightconcept.net>
2024-03-20 21:04:04 -0700

    Reorganize Nixos

--- home/default.nix
+++ home/default.nix
@@ -5,17 +5,36 @@
   config,
   ...
 }: {
-  programs.home-manager.enable = true;
+  programs = {
+    home-manager.enable = true;
+  };
 
-  nixpkgs.config.allowUnfree = true;
-
-  fonts.fontconfig.enable = true;
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = _: true;
+    };
+  };
 
   imports = [
     inputs.nix-colors.homeManagerModules.default
     ./desktops/hyprland
     ./programs
   ];
+
+  # config = {
+  #   my.settings = {
+  #     default = {
+  #       shell = "zsh";
+  #       terminal = "wezterm";
+  #       browser = "firefox";
+  #       editor = "code";
+  #     };
+  #     fonts.monospace = "FiraCode Nerd Font Mono";
+  #   };
+
+  #   colorScheme = inputs.nix-colors.colorSchemes.tokyo-night-terminal-dark;
+  # };
 
   config.colorScheme = inputs.nix-colors.colorSchemes.tokyo-night-terminal-dark;
 
---
+++ hosts/nixos/hardware.nix
@@ -0,0 +1,8 @@
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}: {
+  services.printing.enable = true;
+}
