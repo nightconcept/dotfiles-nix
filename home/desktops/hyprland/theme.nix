5774f8806672fd56fbee527fb3ce5222e8563811
Danny Solivan <dark@nightconcept.net>
2024-03-22 15:19:31 -0700

    feat: update gtk theming overall

--- home/desktops/hyprland/theme.nix
+++ home/desktops/hyprland/theme.nix
@@ -4,9 +4,20 @@
   ];
   gtk = {
     enable = true;
+    iconTheme = {
+      name = "Yaru-magenta-dark";
+      package = pkgs.yaru-theme;
+    };
+
     theme = {
-      name = "tokyo-night-gtk_full";
+      name = "Tokyonight-Dark-B-LB";
       package = pkgs.tokyo-night-gtk;
     };
+
+    cursorTheme = {
+      name = "Bibata-Modern-Classic";
+      package = pkgs.bibata-cursors;
+    };
+  };
   };
 }
