fa04503116137d7bde8524547d7a0e23806ee118
Danny Solivan <dark@nightconcept.net>
2024-03-22 16:05:50 -0700

    chore: change pointer cursor

--- home/desktops/hyprland/theme.nix
+++ home/desktops/hyprland/theme.nix
@@ -2,25 +2,31 @@
   home.packages = with pkgs; [
     # ... other packages
   ];
+
+  home.pointerCursor = {
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 16;
+    gtk.enable = true;
+    x11.enable = true;
+  };
+
   gtk = {
     enable = true;
+
     font = {
       name = "Fira Sans";
       size = 14;
     };
+
     iconTheme = {
-      name = "Papirus-Dark";
-      package = pkgs.papirus-folders;
+      name = "Adwaita";
+      package = pkgs.gnome.adwaita-icon-theme;
     };
 
     theme = {
       name = "Tokyonight-Dark-B-LB";
       package = pkgs.tokyo-night-gtk;
-    };
-
-    cursorTheme = {
-      name = "Bibata-Modern-Classic";
-      package = pkgs.bibata-cursors;
     };
   };
 }
