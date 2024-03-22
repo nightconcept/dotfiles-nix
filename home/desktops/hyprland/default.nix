ef2ea4d2321b3251bcc6c4be549c8f50e074a0fe
Danny Solivan <dark@nightconcept.net>
2024-03-22 16:21:10 -0700

    chore: switch to nautilus

--- home/desktops/hyprland/default.nix
+++ home/desktops/hyprland/default.nix
@@ -16,9 +16,12 @@
   ];
 
   config = {
-    home.sessionVariables = {
-      MOZ_ENABLE_WAYLAND = 1;
-    };
+    home.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" (with pkgs.gst_all_1; [
+      gst-plugins-good
+      gst-plugins-bad
+      gst-plugins-ugly
+      gst-libav
+    ]);
 
     home.packages = with pkgs; [
       brightnessctl
@@ -26,12 +29,19 @@
       pamixer
       wl-clipboard
       # Thunar is very needy and needs all of these and gvfs to get it's full functional powers
-      xfce.thunar
-      xfce.thunar-archive-plugin
-      xfce.thunar-media-tags-plugin
-      xfce.thunar-volman
-      xfce.tumbler
-      xfce.xfconf
+      # xfce.thunar
+      # xfce.thunar-archive-plugin
+      # xfce.thunar-media-tags-plugin
+      # xfce.thunar-volman
+      # xfce.tumbler
+      # xfce.xfconf
+      gnome.gnome-power-manager
+
+      gnome.nautilus
+      ffmpegthumbnailer # thumbnails
+      gnome.nautilus-python # enable plugins
+      gst_all_1.gst-libav # thumbnails
+      nautilus-open-any-terminal # terminal-context-entry
     ];
   };
 }
