ef444103654a212e57593d54f333a4c26cc26f40
Danny Solivan <dark@nightconcept.net>
2024-03-22 16:37:05 -0700

    chore: go back to thunar for now

--- home/desktops/hyprland/default.nix
+++ home/desktops/hyprland/default.nix
@@ -17,20 +17,19 @@
 
   config = {
 
+
     home.packages = with pkgs; [
       brightnessctl
       grimblast
       pamixer
       wl-clipboard
       # Thunar is very needy and needs all of these and gvfs to get it's full functional powers
-      # xfce.thunar
-      # xfce.thunar-archive-plugin
-      # xfce.thunar-media-tags-plugin
-      # xfce.thunar-volman
-      # xfce.tumbler
-      # xfce.xfconf
-      kdePackages.dolphin
-      ffmpegthumbnailer # thumbnails
+      xfce.thunar
+      xfce.thunar-archive-plugin
+      xfce.thunar-media-tags-plugin
+      xfce.thunar-volman
+      xfce.tumbler
+      xfce.xfconf
     ];
   };
 }
