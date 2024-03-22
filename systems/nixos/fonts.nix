e1ab4b8b5bfe2b4eaeed216c5b2bca20e04914dc
Danny Solivan <dark@nightconcept.net>
2024-03-22 16:00:31 -0700

    feat: add lots of fonts and set defaults

--- systems/nixos/fonts.nix
+++ systems/nixos/fonts.nix
@@ -5,24 +5,47 @@
   ...
 }: {
   fonts = {
-    fontconfig.enable = true;
     packages = with pkgs; [
       (
-        fira
         nerdfonts.override
         {
           fonts = [
-            "DroidSansMono"
             "FiraCode"
             "FiraMono"
-            "Hack"
-            "Inconsolata"
-            "Noto"
             "SourceCodePro"
             "Ubuntu"
+            "UbuntuMono"
           ];
         }
       )
+      fira
+      fira-go
+      fira-code-symbols
+      joypixels
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      source-serif
+      work-sans
     ];
+    fontconfig = {
+      antialias = true;
+      defaultFonts = {
+        serif = ["Source Serif"];
+        sansSerif = ["Work Sans" "Fira Sans" "FiraGO"];
+        monospace = ["FiraCode Nerd Font Mono" "SauceCodePro Nerd Font Mono"];
+        emoji = ["Joypixels" "Noto Color Emoji"];
+      };
+      enable = true;
+      hinting = {
+        autohint = false;
+        enable = true;
+        style = "slight";
+      };
+      subpixel = {
+        rgba = "rgb";
+        lcdfilter = "light";
+      };
+    };
   };
 }
