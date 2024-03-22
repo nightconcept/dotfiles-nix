36d5c272d3ded97f5f70a0ed9afbba2e6a8909a2
Danny Solivan <dark@nightconcept.net>
2024-03-22 16:16:52 -0700

    chore: add more gtk config

--- home/desktops/hyprland/gtk.nix
+++ home/desktops/hyprland/gtk.nix
@@ -24,5 +24,34 @@
       name = "Tokyonight-Dark-B-LB";
       package = pkgs.tokyo-night-gtk;
     };
+
+    gtk3.extraConfig = {
+      gtk-toolbar-style = "GTK_TOOLBAR_BOTH";
+      gtk-toolbar-icon-size = "GTK_ICON_SIZE_LARGE_TOOLBAR";
+      gtk-decoration-layout = "appmenu:none";
+      gtk-button-images = 1;
+      gtk-menu-images = 1;
+      gtk-enable-event-sounds = 0;
+      gtk-enable-input-feedback-sounds = 0;
+      gtk-xft-antialias = 1;
+      gtk-xft-hinting = 1;
+      gtk-xft-hintstyle = "hintfull";
+      gtk-error-bell = 0;
+      gtk-application-prefer-dark-theme = true;
+      gtk-recent-files-max-age = 0;
+      gtk-recent-files-limit = 0;
+    };
+
+    gtk4.extraConfig = {
+      gtk-decoration-layout = "appmenu:none";
+      gtk-enable-event-sounds = 0;
+      gtk-enable-input-feedback-sounds = 0;
+      gtk-xft-antialias = 1;
+      gtk-xft-hinting = 1;
+      gtk-xft-hintstyle = "hintfull";
+      gtk-error-bell = 0;
+      gtk-application-prefer-dark-theme = true;
+      gtk-recent-files-max-age = 0;
+    };
   };
 }
