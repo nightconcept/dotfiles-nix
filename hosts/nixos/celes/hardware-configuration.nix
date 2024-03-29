16fb21a77b3effe5a053c2313efc5098614f03a2
Danny Solivan <dark@nightconcept.net>
2024-03-29 07:23:59 -0700

    chore: readd amdgpu to celes

--- hosts/nixos/celes/hardware-configuration.nix
+++ hosts/nixos/celes/hardware-configuration.nix
@@ -7,8 +7,6 @@
 }: {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
-    ./amd-gpu.nix
-    ./amd-pstate.nix
   ];
 
   boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
