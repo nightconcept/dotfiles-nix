018929349a528241af77b221b321e945ef3d22cb
Danny Solivan <dark@nightconcept.net>
2024-03-29 07:02:17 -0700

    refactor: darwin modules

--- systems/darwin/default.nix
+++ systems/darwin/default.nix
@@ -5,114 +5,11 @@
   ...
 }: {
   imports = [
+    ./fonts.nix
+    ./homebrew.nix
+    ./macos.nix
+    ./nix.nix
+    ./pkgs.nix
     ./users.nix
   ];
-
-  # Nix settings, auto cleanup and enable flakes
-  nix = {
-    settings.auto-optimise-store = true;
-    settings.allowed-users = ["danny"];
-    gc = {
-      automatic = true;
-      user = "danny";
-      options = "--delete-older-than 7d";
-    };
-    extraOptions =
-      ''
-        experimental-features = nix-command flakes
-        keep-outputs = true
-        keep-derivations = true
-      ''
-      + lib.optionalString (pkgs.system == "aarch64-darwin") ''
-        extra-platforms = x86_64-darwin aarch64-darwin
-      '';
-  };
-
-  nixpkgs.config.allowUnfree = true;
-
-  # Use homebrew to install casks and Mac App Store apps
-  homebrew = {
-    enable = true;
-    onActivation = {
-      autoUpdate = true;
-      cleanup = "zap";
-      upgrade = true;
-    };
-
-    casks = [
-      "calibre"
-      "eloston-chromium"
-      "ferdium"
-      "firefox"
-      "github"
-      "hiddenbar"
-      "libreoffice"
-      "mos"
-      "nomachine"
-      "notunes"
-      "plex"
-      "raycast"
-      "rectangle"
-      "spotify"
-      "stretchly"
-      "typora"
-      "vlc"
-      "zoom"
-    ];
-  };
-
-  services.nix-daemon.enable = true;
-
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    enableSyntaxHighlighting = true;
-  };
-
-  environment.systemPackages = with pkgs; [
-    bat
-    btop
-    curl
-    gcc
-    gh
-    git
-    home-manager
-    wget
-    vim
-    wget
-    zsh
-  ];
-
-  fonts = {
-    fonts = with pkgs; [
-      (
-        nerdfonts.override
-        {
-          fonts = [
-            "DroidSansMono"
-            "FiraCode"
-            "FiraMono"
-            "Hack"
-            "Inconsolata"
-            "Noto"
-            "SourceCodePro"
-            "Ubuntu"
-          ];
-        }
-      )
-    ];
-  };
-
-  system = {
-    defaults = {
-      finder = {
-        FXDefaultSearchScope = "SCcf";
-        AppleShowAllExtensions = true;
-        FXEnableExtensionChangeWarning = false;
-        ShowStatusBar = true;
-      };
-    };
-  };
-
-  system.stateVersion = 4;
 }
