31b39059af4d792b58bb4b76ed0be2693af7aa53
Danny Solivan <dark@nightconcept.net>
2024-04-11 11:00:09 -0700

    feat: add setup script for cloud

---
+++ nixos-setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# assumed you decrypted already
+
+# partial reference: https://github.com/vimjoyer/impermanent-setup
+
+nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /home/nixos/dotfiles-nix/hosts/nixos/cloud/disks.nix
+
+sudo nixos-install --root /mnt --flake /home/nixos/dotfiles-nix/#cloud
