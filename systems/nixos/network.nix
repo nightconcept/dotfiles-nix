25eabada9b8ae3aefd7e49d139eea5b721ebd07f
Danny Solivan <dark@nightconcept.net>
2024-05-30 16:10:29 -0700

    chore: add aerith

--- systems/nixos/network.nix
+++ systems/nixos/network.nix
@@ -6,8 +6,6 @@
 }: {
   networking.networkmanager = {
     enable = true;
-<<<<<<< HEAD
-=======
   };
 
   # Network mounts
@@ -38,6 +36,5 @@
       # this line prevents hanging on network split
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,user,users";
     in ["${automount_opts},credentials=/etc/nixos/mog-secrets,uid=1000,gid=100"];
->>>>>>> 3349a23 (chore: add aerith)
   };
 }
