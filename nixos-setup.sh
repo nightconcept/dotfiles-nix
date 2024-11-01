b7a71e4b354d8220a362f32745014e1aa736d224
Danny Solivan <dark@nightconcept.net>
2024-10-31 20:56:31 -0700

    feat: Remove nixos-setup script that used disko

--- nixos-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# assumed you decrypted already
-
-# partial reference: https://github.com/vimjoyer/impermanent-setup
-
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /home/nixos/dotfiles-nix/hosts/nixos/cloud/disks.nix
-
-sudo nixos-install --root /mnt --flake /home/nixos/dotfiles-nix/#cloud
