c1a099cc81acedfbe7282d7e37866024f63d8e09
Danny Solivan <dark@nightconcept.net>
2025-03-27 16:02:03 -0700

    fix: Add curl to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,9 @@
 # https://install.determinate.systems/
 # https://nix-community.github.io/home-manager/
 
+# install curl
+sudo apt-get install curl -y
+
 # install nix
 curl --proto '=https' --tlsv1.2 -sSf -L \
   https://install.determinate.systems/nix | sh -s -- install
