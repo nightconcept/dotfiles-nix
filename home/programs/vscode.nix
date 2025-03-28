0b3ca77951a47c9eee068bdc584db9c35a3c552b
Danny Solivan <dark@nightconcept.net>
2025-03-28 10:37:07 -0700

    feat: Add prettier settings for VSCode

--- home/programs/vscode.nix
+++ home/programs/vscode.nix
@@ -12,56 +12,56 @@
         enableUpdateCheck = false;
         enableExtensionUpdateCheck = false;
         extensions = with pkgs.vscode-extensions;
-        [
-          yzhang.markdown-all-in-one
-          bbenoist.nix
-          eamodio.gitlens
-          kamikillerto.vscode-colorize
-          pkief.material-icon-theme
-          ms-python.python
-          ms-vscode-remote.remote-ssh-edit
-          svelte.svelte-vscode
-          elixir-lsp.vscode-elixir-ls
-          ms-vscode-remote.remote-wsl
-          tamasfe.even-better-toml
-          esbenp.prettier-vscode
-        ]
-        ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-          {
-            name = "where-am-i";
-            publisher = "antfu";
-            version = "0.2.0";
-            sha256 = "sha256-M9TCILD6KKLHCDBP0mBR5soeYb2MFuBAmyKPlKbl1tg=";
-          }
-          {
-            name = "tokyo-night-henrikvilhelmberglund";
-            publisher = "henrikvilhelmberglund";
-            version = "1.1.0";
-            sha256 = "sha256-WEdmyIwmJ2uigNhksgzr6IH4PIWwthJvX5N1OG3JUZ4=";
-          }
-          {
-            name = "remote-explorer";
-            publisher = "ms-vscode";
-            version = "0.5.2024081309";
-            sha256 = "sha256-YExf9Yyo7Zp0Nfoap8Vvtas11W9Czslt55X9lb/Ri3s=";
-          }
-          {
-            name = "remote-ssh";
-            publisher = "ms-vscode-remote";
-            version = "0.116.2024100715";
-            sha256 = "sha256-Mo11BGA27Bi62JRPU6INOq3SXTsp5ASYzd8ihlV3ZZY=";
-          }
-          {
-            name = "geminicodeassist";
-            publisher = "google";
-            version = "2.29.0";
-            sha256 = "sha256-CixHqIUgTju8GnH2gwdgYb4UfJ/2Jx8lH5oHzsXqZYk=";
-          }
-        ];
+          [
+            yzhang.markdown-all-in-one
+            bbenoist.nix
+            eamodio.gitlens
+            kamikillerto.vscode-colorize
+            pkief.material-icon-theme
+            ms-python.python
+            ms-vscode-remote.remote-ssh-edit
+            svelte.svelte-vscode
+            elixir-lsp.vscode-elixir-ls
+            ms-vscode-remote.remote-wsl
+            tamasfe.even-better-toml
+            esbenp.prettier-vscode
+          ]
+          ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+            {
+              name = "where-am-i";
+              publisher = "antfu";
+              version = "0.2.0";
+              sha256 = "sha256-M9TCILD6KKLHCDBP0mBR5soeYb2MFuBAmyKPlKbl1tg=";
+            }
+            {
+              name = "tokyo-night-henrikvilhelmberglund";
+              publisher = "henrikvilhelmberglund";
+              version = "1.1.0";
+              sha256 = "sha256-WEdmyIwmJ2uigNhksgzr6IH4PIWwthJvX5N1OG3JUZ4=";
+            }
+            {
+              name = "remote-explorer";
+              publisher = "ms-vscode";
+              version = "0.5.2024081309";
+              sha256 = "sha256-YExf9Yyo7Zp0Nfoap8Vvtas11W9Czslt55X9lb/Ri3s=";
+            }
+            {
+              name = "remote-ssh";
+              publisher = "ms-vscode-remote";
+              version = "0.116.2024100715";
+              sha256 = "sha256-Mo11BGA27Bi62JRPU6INOq3SXTsp5ASYzd8ihlV3ZZY=";
+            }
+            {
+              name = "geminicodeassist";
+              publisher = "google";
+              version = "2.29.0";
+              sha256 = "sha256-CixHqIUgTju8GnH2gwdgYb4UfJ/2Jx8lH5oHzsXqZYk=";
+            }
+          ];
         userSettings = {
           "colorize.languages" = [
-              "nix"
-              "rasi"
+            "nix"
+            "rasi"
           ];
           "editor.accessibilitySupport" = "off";
           "editor.guides.bracketPairs" = true;
@@ -90,7 +90,19 @@
           "workbench.colorTheme" = "Tokyo Night";
           "workbench.iconTheme" = "material-icon-theme";
           "workbench.startupEditor" = "none";
-          
+          "[nix]" = [
+            {
+              "editor.defaultFormatter" = "kamadorueda.alejandra";
+              "editor.formatOnPaste" = true;
+              "editor.formatOnSave" = true;
+              "editor.formatOnType" = false;
+            }
+          ];
+          "[lua]" = [
+            {
+              "editor.defaultFormatter" = "sumneko.lua";
+            }
+          ];
         };
       };
     };
