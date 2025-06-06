285b2ab6c12c1b71b2a4dc8daea24922256fec17
Danny Solivan <dark@nightconcept.net>
2025-06-05 22:57:12 -0700

    fix: udev rules

--- systems/nixos/hardware/default.nix
+++ systems/nixos/hardware/default.nix
@@ -9,7 +9,6 @@
   # Automount USB drives
   services.udev.enable = true;
   services.udev.extraRules = ''
-    ACTION=="add", SUBSYSTEMS=="usb", SUBSYSTEM=="block", ENV{ID_FS_USAGE}=="filesystem",
-    RUN{program} += "${pkgs.systemd}/bin/systemd-mount --no-block --automount=yes --collect $devnode /media
+    ACTION=="add", SUBSYSTEMS=="usb", SUBSYSTEM=="block", ENV{ID_FS_USAGE}=="filesystem", RUN{program}+="${pkgs.systemd}/bin/systemd-mount --no-block --automount=yes --collect $devnode /media"
   '';
 }
