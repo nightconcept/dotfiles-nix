f6b143d51c44bbefd11304b539e774d362c388df
Danny Solivan <dark@nightconcept.net>
2025-09-10 15:02:13 -0700

    chore: revert Aerith and cleanup

--- hosts/nixos/aerith/default.nix
+++ hosts/nixos/aerith/default.nix
@@ -12,8 +12,8 @@
 
   networking.hostName = "aerith";
 
-  # Kernel specified at 6.6 for the latest LTS
-  boot.kernelPackages = pkgs.linuxPackages_6_6;
+  # Kernel specified at 6.12 for the latest LTS
+  boot.kernelPackages = pkgs.linuxPackages_6_12;
 
   # Display settings
   services.xserver.enable = true;
@@ -22,19 +22,11 @@
     enable = true;
     openFirewall = true;
     user = "danny";
-    package = pkgs.plex.overrideAttrs (old: rec {
-      version = "1.42.1.10060-4e8b05daf";
-      src = pkgs.fetchurl {
-        url = "https://downloads.plex.tv/plex-media-server-new/${version}/debian/plexmediaserver_${version}_amd64.deb";
-        sha256 = "1x4ph6m519y0xj2x153b4svqqsnrvhq9n2cxjl50b9h8dny2v0is";
-      };
-    });
   };
   networking.firewall.allowedTCPPorts = [
-    32400
-    1900
     5353
     8324
+    32400
     32410
     32412
     32413
