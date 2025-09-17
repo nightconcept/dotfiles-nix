a2d4080c2d27afc88883d1e806b282e00e72f25f
Danny Solivan <dark@nightconcept.net>
2025-09-17 15:30:34 -0700

    fix: Enable systemd initrd for Plymouth LUKS password prompt
    - Enable boot.initrd.systemd.enable for better Plymouth integration
    - Add plymouth.ignore-serial-consoles to improve password prompt visibility
    - Document splash parameter issue (can be removed if prompt is delayed)
    - This should allow graphical LUKS password entry instead of text mode

--- systems/nixos/bootloader.nix
+++ systems/nixos/bootloader.nix
@@ -3,6 +3,9 @@
   boot.loader.systemd-boot.configurationLimit = 10;  # Show last 10 generations
   boot.loader.efi.canTouchEfiVariables = true;
   
+  # Enable systemd in initrd for better Plymouth integration with LUKS
+  boot.initrd.systemd.enable = true;
+  
   # Plymouth boot splash
   boot.plymouth = {
     enable = true;
@@ -10,8 +13,17 @@
     # Alternative themes: "spinner", "script", "fade-in", "text", "details"
   };
   
-  # Ensure quiet boot for cleaner Plymouth experience
-  boot.kernelParams = [ "quiet" "splash" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3" ];
+  # Kernel parameters for Plymouth with LUKS
+  # Note: Remove "splash" if graphical password prompt doesn't appear
+  boot.kernelParams = [ 
+    "quiet" 
+    "splash"  # Comment out this line if password prompt is delayed
+    "loglevel=3" 
+    "rd.systemd.show_status=false" 
+    "rd.udev.log_level=3" 
+    "udev.log_priority=3" 
+    "plymouth.ignore-serial-consoles"  # Helps with password prompt visibility
+  ];
   boot.consoleLogLevel = 0;
   boot.initrd.verbose = false;
 }
