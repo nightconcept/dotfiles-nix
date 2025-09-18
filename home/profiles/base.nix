5ec63b488325d188c37246cc9970b5eed67c5d25
Danny Solivan <dark@nightconcept.net>
2025-09-18 16:35:55 -0700

    feat: Add modular home configuration system with common programs
    - Create modules/home structure for modular home-manager configs
    - Add common programs module with just package included
    - Update base profile to use new module system
    - Remove old home/common.nix in favor of modular approach

--- home/profiles/base.nix
+++ home/profiles/base.nix
@@ -3,13 +3,17 @@
 
 {
   imports = [
-    ../common.nix
+    ../../modules/home
     ../programs/direnv.nix
     ../programs/git.nix
     ../programs/helix.nix
     ../programs/ssh.nix
     ../secrets/sops.nix
   ];
+
+  modules.home = {
+    programs.common.enable = true;
+  };
 
   home = {
     username = "danny";
